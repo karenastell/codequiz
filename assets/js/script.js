ee5564c9d9b90514870e2e3632ff582a13707d11
Karen Astell <KarenAstell@gmail.com>
2020-04-21 10:58:18 -0400

    added question and answer object in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,42 @@ function setTime() {
 setTime();
 
 
+var questionsAnswers = {
+
+    questions: [
+        "No one can feed garbage to pigs without first obtaining a permit.",
+        "A frog that dies during a frog-jumping contest cannot be eaten and must be destroyed as soon as possible.",
+        "Every legislator, public officer, and lawyer must take an oath stating that they have not fought a duel with deadly weapons.",
+        "Singing or playing only part of the national anthem or remixing it as dance music is punishable by a fine of not more than $100.",
+        "Profanity in public could land a person in jail for up to 30 days. It's illegal for anyone to use vulgar or obscene language in the presence of two or more people.",
+        "It's illegal to carry away or collect seaweed at night.",
+        "Leaving a container of urine or fecal matter on the side of the road is a Class A misdemeanor. And you can't throw it from the vehicle either.",
+        "Someone who bites off another person's limb will face no more than 20 years in prison (but no less than one) — and only if they maimed the victim on purpose.",
+        "We know that kids can be annoying but please remember that it is a felony to sell your children."
+
+
+
+
+    ],
+
+    answers: [
+        "Arizona",
+        "California",
+        "Kentucky",
+        "Massachusetts",
+        "Mississippi",
+        "New Hampshire",
+        "Oregon",
+        "Rhode Island",
+        "Utah",
+        "Florida"
+    ],
+
+    states: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+};
+
+
+
 
 
 // script for highscores.html
@@ -50,8 +86,9 @@ backBtn.addEventListener("click", function (event) {
 });
 
 clearBtn.addEventListener("click", function (event) {
-
     event.preventDefault();
     localStorage.clear();
 });
+
+
 
