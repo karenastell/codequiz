38fef6975f4c47d1363dc5347e33a810a76cc2f9
Karen Astell <KarenAstell@gmail.com>
2020-04-21 09:51:17 -0400

    added a timer to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
 // script for index.html
 
+var timeEl = document.querySelector(".timer");
+
+var secondsLeft = 30;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time Left: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            //sendMessage();
+        }
+
+    }, 1000);
+}
+
+// function sendMessage() {
+//   timeEl.textContent = " ";
+
+//   var imgEl = document.createElement("img");
+
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+setTime();
 
 
 
