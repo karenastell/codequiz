0315a21ba6ddbdb40fabba06b6ba09651a8dbb11
Karen Astell <KarenAstell@gmail.com>
2020-04-26 15:26:28 -0400

    debugging local storage issue on github

--- assets/js/hsscript.js
+++ assets/js/hsscript.js
@@ -1,18 +1,25 @@
-var showHighScores = JSON.parse(localStorage.getItem("scores"));
-var tableBodyElement = $(".score-body");
+function displayHighScores() {
 
-showHighScores.forEach(function (score) {
-    // adds local storage info to the table element
-    var tableRowElement = $("<tr>");
-    var scoreTd = $("<td>").text(score.score);
-    var nameTd = $("<td>").text(score.name);
-    tableRowElement.append(nameTd);
-    tableRowElement.append(scoreTd);
-    tableBodyElement.append(tableRowElement);
-});
+    var showHighScores = JSON.parse(localStorage.getItem("scores"));
+    var tableBodyElement = $(".score-body");
+    if (showHighScores === null) {
+        $(".score-body").text("");
+    } else {
+        showHighScores.forEach(function (score) {
+            // adds local storage info to the table element
+            var tableRowElement = $("<tr>");
+            var scoreTd = $("<td>").text(score.score);
+            var nameTd = $("<td>").text(score.name);
+            tableRowElement.append(nameTd);
+            tableRowElement.append(scoreTd);
+            tableBodyElement.append(tableRowElement);
+        });
+    }
+}
 
+displayHighScores();
 
 $(".clear-score").on("click", function () {
     localStorage.clear();
-    tableBodyElement.remove();
+    displayHighScores();
 });
