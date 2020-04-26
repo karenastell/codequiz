e3409f911fceb3ee14d4851c6144e02c0599073e
Karen Astell <KarenAstell@gmail.com>
2020-04-26 15:49:27 -0400

    debugging github issues

--- assets/js/hsscript.js
+++ assets/js/hsscript.js
@@ -1,13 +1,16 @@
+
+var tableBodyElement;
 function displayHighScores() {
 
     var showHighScores = JSON.parse(localStorage.getItem("scores"));
     var tableBodyElement = $(".score-body");
+    var tableRowElement;
     if (showHighScores === null) {
         $(".score-body").text("");
     } else {
         showHighScores.forEach(function (score) {
             // adds local storage info to the table element
-            var tableRowElement = $("<tr>");
+            tableRowElement = $("<tr>");
             var scoreTd = $("<td>").text(score.score);
             var nameTd = $("<td>").text(score.name);
             tableRowElement.append(nameTd);
